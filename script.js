4108efb067060d29801bea154ae134c7ef510ed8
Joel Kustemo <kustemo.joel@elev.ga.ntig.se>
2021-10-29 13:27:27 +0200

    sidebar menu

---
+++ script.js
@@ -0,0 +1,7 @@
+function openSidebar() {
+    document.getElementById("menu").style.display = "block";
+  }
+  
+  function closeSidebar() {
+    document.getElementById("menu").style.display = "none";
+  }
